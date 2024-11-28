62fbc12f711014386c7ba85a3b1a826543119075
Feliphe <chavesfeliphe@gmail.com>
2024-11-28 14:19:26 -0300

    28112924 brokul

--- data-otservbr-global/scripts/quests/the_secret_library_quest/liquid_death/actions_brokulLever.lua
+++ data-otservbr-global/scripts/quests/the_secret_library_quest/liquid_death/actions_brokulLever.lua
@@ -4,7 +4,7 @@ local config = {
 		position = Position(33483, 31437, 15),
 	},
 	timeToFightAgain = 20 * 60 * 60,
-	minPlayers = 5,
+	minPlayers = 1,
 	playerPositions = {
 		{ pos = Position(33522, 31465, 15), teleport = Position(33484, 31446, 15) },
 		{ pos = Position(33520, 31465, 15), teleport = Position(33484, 31446, 15) },
@@ -16,15 +16,7 @@ local config = {
 		from = Position(33472, 31427, 15),
 		to = Position(33496, 31450, 15),
 	},
-	exit = Position(33528, 31464, 14),
-	onUseExtra = function(creature, infoPositions)
-		if creature and creature:isPlayer() then
-			if creature:getStorageValue(Storage.Quest.U11_80.TheSecretLibrary.LiquidDeath.Questline) >= 6 then
-				return true
-			end
-			return false
-		end
-	end,
+	exit = Position(33528, 31464, 14)
 }
 
 local leverBrokul = BossLever(config)
