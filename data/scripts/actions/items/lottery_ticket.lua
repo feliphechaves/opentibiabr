bc2d3d5003b3df469c566c5908216927450282b5
Feliphe <chavesfeliphe@gmail.com>
2025-04-04 13:59:15 -0300

    feat: increase lottery ticket win chance to 20%

--- data/scripts/actions/items/lottery_ticket.lua
+++ data/scripts/actions/items/lottery_ticket.lua
@@ -1,7 +1,7 @@
 local lotteryTicket = Action()
 
 function lotteryTicket.onUse(player, item, fromPosition, target, toPosition, isHotkey)
-	if math.random(98) == 1 then
+	if math.random(5) == 1 then -- 20% de chance
 		player:getPosition():sendMagicEffect(CONST_ME_SOUND_YELLOW)
 		item:transform(5958)
 	else
