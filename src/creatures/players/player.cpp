846473e74c37e7e6741cce6814431a865f1a2aa2
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2025-05-12 21:29:26 -0300

    fix: getItem crash

--- src/creatures/players/player.cpp
+++ src/creatures/players/player.cpp
@@ -4849,7 +4849,8 @@ void Player::stashContainer(const StashContainerList &itemDict) {
 			}
 		} else {
 			if (const auto &parent = item->getParent()) {
-				if (parent && parent->getItem()->getID() == ITEM_BROWSEFIELD) {
+				const auto &parentItem = parent->getItem();
+				if (parentItem && parentItem->getID() == ITEM_BROWSEFIELD) {
 					const auto &parentTile = parent->getTile();
 					if (parentTile) {
 						parentTile->removeThing(item, itemCount);
