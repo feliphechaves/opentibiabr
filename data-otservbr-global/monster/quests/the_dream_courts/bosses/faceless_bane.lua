68c8be629c9d8084b257d84a8b752f1c00a4015d
Feliphe <chavesfeliphe@gmail.com>
2025-06-11 20:44:51 -0300

    refactor: remove unnecessary event handling and reset logic for Faceless Bane to streamline boss functionality

--- data-otservbr-global/monster/quests/the_dream_courts/bosses/faceless_bane.lua
+++ data-otservbr-global/monster/quests/the_dream_courts/bosses/faceless_bane.lua
@@ -22,7 +22,6 @@ monster.manaCost = 0
 
 monster.events = {
 	"dreamCourtsDeath",
-	"facelessThink",
 }
 
 monster.changeTarget = {
@@ -152,11 +151,6 @@ monster.immunities = {
 
 mType.onSpawn = function(monster, spawnPosition)
 	if monster:getType():isRewardBoss() then
-		-- reset global storage state to default / ensure sqm's reset for the next team
-		Game.setStorageValue(GlobalStorage.TheDreamCourts.FacelessBane.Deaths, -1)
-		Game.setStorageValue(GlobalStorage.TheDreamCourts.FacelessBane.StepsOn, -1)
-		Game.setStorageValue(GlobalStorage.TheDreamCourts.FacelessBane.ResetSteps, 1)
-		monster:registerEvent("facelessBaneImmunity")
 		monster:setReward(true)
 	end
 end
